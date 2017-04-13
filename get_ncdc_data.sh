d6e5d18373844b8dea4e4fafac476bbdb9b11660
Yunkai Feng <yunkaifeng2009@gmail.com>
2017-04-13 15:51:13 +0800

    Create get_ncdc_data.sh

---
+++ get_ncdc_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ 
+CURRENT_DIR=$(cd `dirname $0`; pwd)
+ 
+[ -e $CURRENT_DIR/ncdc ] || mkdir $CURRENT_DIR/ncdc
+[ -e $CURRENT_DIR/ncdc/files ] || mkdir $CURRENT_DIR/ncdc/files
+ 
+for i in `seq 1901 2012`
+do
+    cd $CURRENT_DIR/ncdc/
+    wget -r -np -nH .cut-dirs=3 -R index.html ftp://ftp.ncdc.noaa.gov/pub/data/noaa/$i/
+    cd pub/data/noaa/$i/
+    cp *.gz $CURRENT_DIR/ncdc/files
+done
