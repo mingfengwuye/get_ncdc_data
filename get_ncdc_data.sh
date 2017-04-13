515396bd109723300871ed638ea1587ef2f8a980
Yunkai Feng <yunkaifeng2009@gmail.com>
2017-04-13 15:53:56 +0800

    Update get_ncdc_data.sh

--- get_ncdc_data.sh
+++ get_ncdc_data.sh
@@ -5,7 +5,7 @@ CURRENT_DIR=$(cd `dirname $0`; pwd)
 [ -e $CURRENT_DIR/ncdc ] || mkdir $CURRENT_DIR/ncdc
 [ -e $CURRENT_DIR/ncdc/files ] || mkdir $CURRENT_DIR/ncdc/files
  
-for i in `seq 1901 2012`
+for i in `seq 1901 2017`
 do
     cd $CURRENT_DIR/ncdc/
     wget -r -np -nH .cut-dirs=3 -R index.html ftp://ftp.ncdc.noaa.gov/pub/data/noaa/$i/
